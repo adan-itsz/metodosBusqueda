d691bafb0fb1944aaa1bea570ff9592557e7b7a7
Adan De Santiago <adan1995a@gmail.com>
2015-09-30 23:24:15 -0500

    amos metodos correctos

---
+++ metodosBusqueda/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace metodosBusqueda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAgregar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnElementos = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecuencial = new System.Windows.Forms.TextBox();
+            this.txtBinaria = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(223, 68);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtCantidad.TabIndex = 0;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(213, 259);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 22);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(357, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 277);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txtAgregar
+            // 
+            this.txtAgregar.Location = new System.Drawing.Point(223, 161);
+            this.txtAgregar.Name = "txtAgregar";
+            this.txtAgregar.Size = new System.Drawing.Size(100, 22);
+            this.txtAgregar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero de elementos";
+            // 
+            // btnElementos
+            // 
+            this.btnElementos.Location = new System.Drawing.Point(223, 109);
+            this.btnElementos.Name = "btnElementos";
+            this.btnElementos.Size = new System.Drawing.Size(100, 23);
+            this.btnElementos.TabIndex = 6;
+            this.btnElementos.Text = "Aceptar";
+            this.btnElementos.UseVisualStyleBackColor = true;
+            this.btnElementos.Click += new System.EventHandler(this.btnElementos_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(41, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Agregar número";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(50, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Buscar número";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "cantidad de intentos";
+            // 
+            // txtSecuencial
+            // 
+            this.txtSecuencial.Location = new System.Drawing.Point(41, 396);
+            this.txtSecuencial.Name = "txtSecuencial";
+            this.txtSecuencial.ReadOnly = true;
+            this.txtSecuencial.Size = new System.Drawing.Size(100, 22);
+            this.txtSecuencial.TabIndex = 10;
+            // 
+            // txtBinaria
+            // 
+            this.txtBinaria.Location = new System.Drawing.Point(287, 396);
+            this.txtBinaria.Name = "txtBinaria";
+            this.txtBinaria.ReadOnly = true;
+            this.txtBinaria.Size = new System.Drawing.Size(100, 22);
+            this.txtBinaria.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Busqueda secuencial";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(265, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Busqueda binaria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 440);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBinaria);
+            this.Controls.Add(this.txtSecuencial);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnElementos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtCantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnElementos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecuencial;
+        private System.Windows.Forms.TextBox txtBinaria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ metodosBusqueda/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace metodosBusqueda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("","");
+        }
+        int cantidadElementos;
+        int conta = 0;
+        int[] array;
+        int numBuscar;
+
+
+        private void btnElementos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cantidadElementos = Convert.ToInt32(txtCantidad.Text);
+                array = new int[cantidadElementos];
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+                txtCantidad.Clear();
+                txtAgregar.Focus();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Llenar correctamente");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)// agregar numero
+        {
+            try
+            {
+                if (conta < array.Length)
+                {
+                    if (conta == 0)
+                    {
+                        dataGridView1[0, conta].Value = txtAgregar.Text;
+                        array[conta] = Convert.ToInt32(txtAgregar.Text);
+                        MessageBox.Show("agregado correctamente");
+                        txtAgregar.Clear();
+                        txtAgregar.Focus();
+                        conta++;
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(txtAgregar.Text) > array[conta - 1])
+                        {
+                            array[conta] = Convert.ToInt32(txtAgregar.Text);
+                            dataGridView1[0, conta].Value = txtAgregar.Text;
+                            MessageBox.Show("agregado correctamente");
+                            txtAgregar.Clear();
+                            txtAgregar.Focus();
+                            conta++;
+                        }
+                        else
+                        {
+                            MessageBox.Show("debe colocar un numero mayor al anterior");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("el arreglo se llenó");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Llenar correctamente");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtSecuencial.Text= Convert.ToString (busquedaSecuencial());
+            txtBinaria.Text= Convert.ToString (busquedaBinaria());
+
+        }
+        public int busquedaSecuencial()
+        {
+            int i = 0;
+           numBuscar= Convert.ToInt32 (txtBuscar.Text);
+            for ( ; i < array.Length; i++)
+            {
+                if (array[i] == numBuscar)
+                {
+                    i++;
+                    MessageBox.Show("el numero" + numBuscar + " " + " si se encuentra");
+                    return i;
+                }
+                else
+                {
+                    
+                }
+                
+            }
+            MessageBox.Show(" no se encontro el numero");
+            return i;
+        }
+        public int busquedaBinaria()
+        {
+            numBuscar= Convert.ToInt32 (txtBuscar.Text);
+            int inicio=0;
+            int fin = array.Length -1;
+            int centro;
+            int contador=0;
+            while (inicio >= 0 || fin <= array.Length - 1)
+            {
+                centro = (inicio + fin) / 2;
+                if (numBuscar == array[centro])
+                {
+                    contador++;
+                    MessageBox.Show("el numero" + numBuscar + " " + " si se encuentra");
+                    return contador;
+ 
+                }
+                else
+                {
+                    if (numBuscar > array[centro])
+                    {
+                        inicio = centro + 1;
+                    }
+                    else
+                    {
+                        fin = centro - 1;
+                    }
+                }
+                contador++;
+            }
+           
+            MessageBox.Show(" no se encontro el numero");
+            return contador;
+        }
+    }
+}
